bf18e3ea319f530775951411e22ce0c2fe4c0782
Anton <antonafonin154@gmail.com>
2024-05-04 17:18:37 -0400

    finished project

---
+++ hangman/src/DrawHangman.java
@@ -0,0 +1,69 @@
+public class DrawHangman {
+    public void drawHangman(int mistakes) {
+        switch (mistakes) {
+            case 0 -> System.out.println("""
+                    ___________               
+                    |   \\    |
+                    |  
+                    |  
+                    |  
+                    |
+                    ~~~~~~~~~~~
+                    """);
+            case 1 -> System.out.println("""
+                    ___________               
+                    |   \\    |
+                    |   ()
+                    |  
+                    |  
+                    |
+                    ~~~~~~~~~~~
+                    """);
+            case 2 -> System.out.println("""
+                    ___________                
+                    |   \\    |
+                    |   ()
+                    |   ||
+                    |  
+                    |
+                    ~~~~~~~~~~~
+                    """);
+            case 3 -> System.out.println("""
+                    ___________                
+                    |   \\    |
+                    |   ()
+                    |  /||
+                    |  
+                    |
+                    ~~~~~~~~~~~
+                    """);
+            case 4 -> System.out.println("""
+                    ___________                
+                    |   \\    |
+                    |   ()
+                    |  /||\\
+                    |  
+                    |
+                    ~~~~~~~~~~~
+                    """);
+            case 5 -> System.out.println("""
+                    ___________                
+                    |   \\    |
+                    |   ()
+                    |  /||\\
+                    |  /   
+                    |
+                    ~~~~~~~~~~~
+                    """);
+            case 6 -> System.out.println("""
+                    ___________               \s
+                    |   \\    |
+                    |   ()
+                    |  /||\\
+                    |  /  \\
+                    |
+                    ~~~~~~~~~~~
+                   \s""");
+        }
+    }
+}
--- hangman/src/Main.java
+++ hangman/src/Main.java
@@ -1,5 +1,56 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        gameLoop();
+    }
+
+    public static void gameLoop() throws IOException {
+        StringBuilder hidden = new StringBuilder();
+        DrawHangman hangman = new DrawHangman();
+        Scanner sc = new Scanner(System.in);
+        String randomWord = getRandomWord().toLowerCase();
+        hidden.append("*".repeat(randomWord.length()));
+        int mistakes = 0;
+
+        System.out.println(randomWord);
+
+        while (true) {
+            System.out.print("Input a keyword: ");
+            String keyword = sc.next();
+
+            if (randomWord.contains(keyword)) {
+                for (int i = 0; i < randomWord.length(); i++) {
+                    if (randomWord.charAt(i) == keyword.charAt(0)) {
+                        hidden.replace(i, i + 1, keyword);
+                    }
+                }
+                System.out.println(hidden);
+            } else {
+                hangman.drawHangman(mistakes);
+                mistakes++;
+            }
+
+            if (mistakes == 7) {
+                System.out.println("Sorry but you lost this game");
+                break;
+            }
+
+            if (hidden.toString().equals(randomWord)) {
+                System.out.println("Congratulation! You won this game");
+                break;
+            }
+        }
+    }
+
+    public static String getRandomWord() throws IOException {
+        Random random = new Random();
+        List<String> words = Files.readAllLines(Paths.get("src/wordsDB/words.txt"));
+
+        return words.get(random.nextInt(words.size()));
     }
 }
+
