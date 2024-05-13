356451fdf6784c9851f6c0612ede245db1b8db84
Anton <antonafonin154@gmail.com>
2024-05-13 19:59:20 -0400

    refactor: added constants

--- hangman/src/Main.java
+++ hangman/src/Main.java
@@ -4,6 +4,18 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class Main {
+    private static final String WIN_MESSAGE = "Congratulation! You won this game";
+    private static final String LOSE_MESSAGE = "Sorry but you lost this game";
+    private static final String RANDOM_WORD;
+
+    static {
+        try {
+            RANDOM_WORD = getRandomWord().toLowerCase();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         gameLoop();
     }
@@ -12,19 +24,18 @@ public class Main {
         StringBuilder hidden = new StringBuilder();
         DrawHangman hangman = new DrawHangman();
         Scanner sc = new Scanner(System.in);
-        String randomWord = getRandomWord().toLowerCase();
-        hidden.append("*".repeat(randomWord.length()));
+        hidden.append("*".repeat(RANDOM_WORD.length()));
         int mistakes = 0;
 
-        System.out.println(randomWord);
+        System.out.println(RANDOM_WORD);
 
         while (true) {
             System.out.print("Input a keyword: ");
             String keyword = sc.next();
 
-            if (randomWord.contains(keyword)) {
-                for (int i = 0; i < randomWord.length(); i++) {
-                    if (randomWord.charAt(i) == keyword.charAt(0)) {
+            if (RANDOM_WORD.contains(keyword)) {
+                for (int i = 0; i < RANDOM_WORD.length(); i++) {
+                    if (RANDOM_WORD.charAt(i) == keyword.charAt(0)) {
                         hidden.replace(i, i + 1, keyword);
                     }
                 }
@@ -35,16 +46,24 @@ public class Main {
             }
 
             if (mistakes == 7) {
-                System.out.println("Sorry but you lost this game");
+                System.out.println(LOSE_MESSAGE);
                 break;
             }
 
-            if (hidden.toString().equals(randomWord)) {
-                System.out.println("Congratulation! You won this game");
+            if (hidden.toString().equals(RANDOM_WORD)) {
+                System.out.println(WIN_MESSAGE);
                 break;
             }
         }
     }
+
+//    public String isWin(int mistakes) {
+//        if () {
+//            return LOSE_MESSAGE;
+//        }
+//
+//        return ;
+//    }
 
     public static String getRandomWord() throws IOException {
         Random random = new Random();
