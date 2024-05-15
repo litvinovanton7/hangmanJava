3439c78110477b53a6e78a470e065be5a967b967
Anton <antonafonin154@gmail.com>
2024-05-15 16:22:17 -0400

    i did something

--- hangman/src/Main.java
+++ hangman/src/Main.java
@@ -6,6 +6,10 @@ import java.util.*;
 public class Main {
     private static final String WIN_MESSAGE = "Congratulation! You won this game";
     private static final String LOSE_MESSAGE = "Sorry but you lost this game";
+    private static final String WRONG_KEYWORD_MESSAGE = "Please input a correct letter[a..z]";
+    private static final String USED_WRONG_KEYWORD_MESSAGE = "You already used this letter, and it is the wrong letter.";
+    private static final String USED_CORRECT_KEYWORD_MESSAGE = "You already used this letter, and it is the correct letter.";
+    private static final int DEAD_STATE = 7;
     private static final Hangman HANGMAN = new Hangman();
     private static final String RANDOM_WORD;
 
@@ -17,14 +21,16 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         gameLoop();
     }
 
-    public static void gameLoop() throws IOException {
+    public static void gameLoop() {
         StringBuilder hidden = new StringBuilder();
         Scanner sc = new Scanner(System.in);
         hidden.append("*".repeat(RANDOM_WORD.length()));
+        List<String> wrongKeyword = new ArrayList<>();
+        List<String> usedKeywords = new ArrayList<>();
         int mistakes = 0;
 
         System.out.println(RANDOM_WORD);
@@ -33,19 +39,36 @@ public class Main {
             System.out.print("Input a keyword: ");
             String keyword = sc.next();
 
-            if (RANDOM_WORD.contains(keyword)) {
-                for (int i = 0; i < RANDOM_WORD.length(); i++) {
-                    if (RANDOM_WORD.charAt(i) == keyword.charAt(0)) {
-                        hidden.replace(i, i + 1, keyword);
-                    }
-                }
-                System.out.println(hidden);
-            } else {
-                HANGMAN.drawHangman(mistakes);
-                mistakes++;
+            if (in(wrongKeyword, keyword)) {
+                System.out.println(USED_WRONG_KEYWORD_MESSAGE);
             }
 
-            if (mistakes == 7) {
+            if (isKeyword(keyword)) {
+                if (in(RANDOM_WORD, keyword)) {
+                    for (int i = 0; i < RANDOM_WORD.length(); i++) {
+                        if (RANDOM_WORD.charAt(i) == keyword.charAt(0)) {
+                            hidden.replace(i, i + 1, keyword);
+                        }
+                    }
+                    if (in(usedKeywords, keyword)) {
+                        System.out.println(USED_CORRECT_KEYWORD_MESSAGE);
+                    }
+                    usedKeywords.add(keyword);
+                    System.out.println(hidden);
+                } else {
+                    if (in(wrongKeyword, keyword)) {
+                        continue;
+                    }
+                    wrongKeyword.add(keyword);
+                    HANGMAN.drawHangman(mistakes);
+                    System.out.println(wrongKeyword);
+                    mistakes++;
+                }
+            } else {
+                System.out.println(WRONG_KEYWORD_MESSAGE);
+            }
+
+            if (mistakes == DEAD_STATE) {
                 System.out.println(LOSE_MESSAGE);
                 break;
             }
@@ -57,10 +80,6 @@ public class Main {
         }
     }
 
-    public static boolean isWin(StringBuilder word) {
-        return word.toString().equals(RANDOM_WORD);
-    }
-
     public static String getRandomWord() throws IOException {
         Random random = new Random();
         List<String> words = Files.readAllLines(Paths.get("hangman/src/words.txt"));
@@ -70,6 +89,22 @@ public class Main {
         }
 
         return words.get(random.nextInt(words.size()));
+    }
+
+    public static boolean in(String word, String keyword) {
+        return word.contains(keyword);
+    }
+
+    public static boolean in(List<String> list, String keyword) {
+        return list.contains(keyword);
+    }
+
+    public static boolean isWin(StringBuilder word) {
+        return word.toString().equals(RANDOM_WORD);
+    }
+
+    public static boolean isKeyword(String keyword) {
+        return Character.isLetter(keyword.charAt(0)) && keyword.length() == 1;
     }
 }
 
